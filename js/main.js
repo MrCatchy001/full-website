123524177feb4fa456dc07f033ea1e9dbaa2c1e7
Asser Ibrahim <77065572+MrCatchy001@users.noreply.github.com>
2022-04-21 21:14:03 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var mybutton = document.getElementById("myBtn");
+const opt = document.getElementById("opt")
+const bigcontainer = document.getElementById("big-container")
+
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -16,3 +19,11 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+opt.addEventListener("click", () => {
+
+  bigcontainer.style.display = "flex";
+
+}
+)
